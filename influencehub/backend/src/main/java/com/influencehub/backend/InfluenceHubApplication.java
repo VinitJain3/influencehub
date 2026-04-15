904d9ce01cdc91498e8283fb81258c448c8b7bf3
Anjali <anjali@example.com>
2026-04-16 02:56:44 +0530

    initialized backend application and configuration files

---
+++ influencehub/backend/src/main/java/com/influencehub/backend/InfluenceHubApplication.java
@@ -0,0 +1,13 @@
+package com.influencehub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluenceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluenceHubApplication.class, args);
+	}
+
+}
